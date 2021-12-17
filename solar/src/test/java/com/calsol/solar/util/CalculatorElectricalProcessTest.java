e280c4c7a8eb10a52a03c0e886a7bfc1423c53a1
manuelpope <manuelpope@gmail.com>
2021-12-17 11:03:44 -0300

    re sizing with this loads and check valid output ,failing

--- solar/src/test/java/com/calsol/solar/util/CalculatorElectricalProcessTest.java
+++ solar/src/test/java/com/calsol/solar/util/CalculatorElectricalProcessTest.java
@@ -1,24 +1,13 @@
 package com.calsol.solar.util;
 
 import com.calsol.solar.domain.entity.*;
-import com.google.gson.Gson;
-import org.aspectj.lang.annotation.Before;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.TimeZone;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorElectricalProcessTest {
 
@@ -32,11 +21,13 @@ class CalculatorElectricalProcessTest {
         Panel panel = Panel.builder().voltage(24.0).wattsPk(230).build();
 
         Load l1 = Load.builder().energyDay(160.0).totalEnergy(160.0).energyNight(0.0).power12VoltsDC(20.0).power110VoltsAC(0.0).type("DC").build();
-        Load l2 = Load.builder().energyDay(0.0).energyNight(60.0).totalEnergy(60.0).power110VoltsAC(30.0).power12VoltsDC(0.0).type("AC").build();
+        Load l2 = Load.builder().energyDay(150.0).energyNight(60.0).totalEnergy(210.0).power110VoltsAC(30.0).power12VoltsDC(0.0).type("AC").build();
+        Load l3 = Load.builder().energyDay(0.0).energyNight(200.0).totalEnergy(200.0).power110VoltsAC(190.0).power12VoltsDC(0.0).type("AC").build();
+        Load l4 = Load.builder().energyDay(180.0).energyNight(60.0).totalEnergy(240.0).power110VoltsAC(30.0).power12VoltsDC(0.0).type("AC").build();
 
         design.setPanel(panel);
         design.setCondition(condition);
-        design.setLoadList(Arrays.asList(l1, l2));
+        design.setLoadList(Arrays.asList(l1, l2, l3, l4));
 
 
     }
