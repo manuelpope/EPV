6f44480439ed62258219fccdea9da1ff3bde0a11
manuelpope <manuelpope@gmail.com>
2021-12-28 13:54:16 -0300

    validation order set in http request controller

--- solar/src/main/java/com/calsol/solar/SolarApplication.java
+++ solar/src/main/java/com/calsol/solar/SolarApplication.java
@@ -2,12 +2,16 @@ package com.calsol.solar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * The type Solar application.
  */
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class SolarApplication {
 
     /**
--- solar/src/main/java/com/calsol/solar/service/ContextDesign.java
+++ solar/src/main/java/com/calsol/solar/service/ContextDesign.java
@@ -3,10 +3,19 @@ package com.calsol.solar.service;
 import com.calsol.solar.domain.entity.Design;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TimeZone;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiPredicate;
 
 /**
  * The type Context design.
@@ -15,7 +24,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 @Slf4j
 public class ContextDesign {
-
+    /**
+     * The Local date time entry bi predicate.
+     */
+    final BiPredicate<LocalDateTime, Entry<String, Design>> localDateTimeEntryBiPredicate = (hourAgo1, entry1) -> entry1.getValue().getLocalDateTime().minusHours(1L).isBefore(hourAgo1);
+    private final ZoneId zoneId = TimeZone.getTimeZone("UTC").toZoneId();
     private Map<String, Design> context;
 
     /**
@@ -69,6 +82,41 @@ public class ContextDesign {
         log.info("update " + design.getName());
         assertExistenceDesign(design.getName());
         context.put(design.getName(), design);
+
+    }
+
+    /**
+     * House keeping.
+     *
+     * @throws Exception the exception
+     */
+    @Async
+    public void houseKeeping() throws Exception {
+        log.info("Performing House Keeping of non sync designs");
+        LocalDateTime hourAgo = LocalDateTime.now(this.zoneId).minusHours(1L);
+        List<String> listToCleanDesign = new ArrayList<>();
+        for (Entry<String, Design> entry : this.context.entrySet()) {
+
+            if (localDateTimeEntryBiPredicate.test(hourAgo, entry)) {
+                listToCleanDesign.add(entry.getKey());
+            }
+        }
+        log.info("count elements to be removed: " + listToCleanDesign.size());
+        for (String key : listToCleanDesign) {
+            this.context.remove(key);
+        }
+
+    }
+
+    /**
+     * Schedule fixed delay task.
+     *
+     * @throws Exception the exception
+     */
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public synchronized void scheduleTaskCleaningDecorator() throws Exception {
+        log.info("Fixed Rate task - " + System.currentTimeMillis() / 1000);
+        this.houseKeeping();
 
     }
 }
